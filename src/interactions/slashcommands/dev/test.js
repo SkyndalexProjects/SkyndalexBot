397bb90f872d0747b8ec4d230672480f7899de80
Korrumz2PL <korrumz2@gmail.com>
2022-07-05 19:31:53 +0200

    update test.js

--- src/interactions/slashcommands/dev/test.js
+++ src/interactions/slashcommands/dev/test.js
@@ -1,14 +1,40 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageActionRow, MessageSelectMenu, MessageEmbed } = require("discord.js")
 const { fetch } = require("undici");
-
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("test")
         .setDescription("test command"),
     async execute(client, interaction) {
-        await interaction.guild.members.fetch()
-        console.log(client.users.cache.size)
+        const embed = new MessageEmbed()
+            .setColor('#4ccdea')
+            .setTitle('Select a channel below.')
+            .setTimestamp()
 
-      let nameList = await interaction.guild.roles.cache.get('943888111618887700').members.map(m => m.setNickname("m"));
+        let selectmenu = new MessageActionRow().addComponents(
+            new MessageSelectMenu()
+                .setCustomId('channel')
+                .setPlaceholder('Nothing selected')
+                .addOptions([
+                    {
+                        label: `❌ Cancel`,
+                        description: 'Cancel the channel selection',
+                        value: 'cancel',
+                    },
+                ]),
+        );
+
+        interaction.guild.channels.cache.first(24).map((channel) => {
+            if (["GUILD_NEWS", "GUILD_TEXT"].includes(channel.type)) {
+                selectmenu.components[0].addOptions([
+                    {
+                        label: `📒 ${channel.name}`,
+                        description: `${channel.name}`,
+                        value: `${channel.id}`,
+                    },
+                ]);
+            }
+        })
+        await interaction.reply({ embeds: [embed], components: [selectmenu] });
     }
 }

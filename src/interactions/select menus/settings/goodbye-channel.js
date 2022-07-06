c671ccf9ccdb985a0f1881678bf492f8d866eecc
Korrumz2PL <korrumz2@gmail.com>
2022-07-06 16:28:28 +0200

    settings: goodbye-channel.js

---
+++ src/interactions/select menus/settings/goodbye-channel.js
@@ -0,0 +1,34 @@
+const { MessageEmbed } = require("discord.js");
+module.exports = async (client, interaction) => {
+    if (!interaction.isSelectMenu() || interaction.customId !== "channels_settings" || interaction.values[0] !== "goodbye_channel") return;
+
+    const embed = new MessageEmbed()
+        .setDescription(`Please mention your complaints channel, or enter its ID`)
+        .setColor("BLURPLE")
+    await interaction.reply({ embeds: [embed] })
+
+    let filter =  m => m.author.id === interaction.user.id;
+    const collector = interaction.channel.createMessageCollector({ filter, time: 15000 });
+
+    collector.on('collect', async m => {
+        const channel = m?.mentions?.channels?.first() || m?.guild?.channels?.cache?.get(m?.content)
+        if (!channel?.id) {
+            return interaction.channel.send(`${interaction.user.tag}, It's not channel!`)
+        }
+
+        await r.table("settings").insert({
+            id: interaction.guild.id,
+            goodbyeChannel: channel.id,
+        }, { conflict: "update" }).run(client.con)
+
+        const embedSuccessfully = new MessageEmbed()
+            .setDescription(`Successfully added goodbye channel to database!\nNew value: ${channel.name} (<#${channel.id}>)\nYou have a few seconds to change your channel. `)
+            .setColor("GREEN")
+        await interaction.editReply({ embeds: [embedSuccessfully] });
+    });
+
+    collector.on('end', async collected => {
+        await interaction.editReply( { content: "Your time to complete the options is over.", embeds: [] })
+        collector.stop()
+    });
+}
